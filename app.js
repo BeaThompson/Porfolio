c296acc4a3836f371b93b8cdf9e0222dc5db2c90
BeaThompson <brayden.thompson49@gmail.com>
2022-08-07 16:59:38 -0500

    new file:   app.js 	new file:   contact.html 	new file:   images/codePic.jpg 	new file:   images/codingforLYFE.jpg 	new file:   images/pic1.svg 	new file:   images/pplCode.jpg 	new file:   index.html 	new file:   services.html 	new file:   styles.css 	new file:   tech.html

---
+++ app.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+function showDiv() {
+  document.getElementById("appear").style.display = "block";
+}
